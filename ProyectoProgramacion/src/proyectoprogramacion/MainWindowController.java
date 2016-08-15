90978021977195935586558ff1325c2ef48c0c80
jmoris <jesusmoriis@hotmail.com>
2016-08-15 16:38:20 -0300

    Solución al selector de ficheros.
    Se soluciono la excepción del selector de ficheros, ahora funciona
    correctamente el selector y se puede obtener la información de dicho
    archivo.
    Se activo el filtro de extensiones y se agrego la extension
    MMG(MumoGrafo) como extension por defecto de los grafos.

--- ProyectoProgramacion/src/proyectoprogramacion/MainWindowController.java
+++ ProyectoProgramacion/src/proyectoprogramacion/MainWindowController.java
@@ -5,19 +5,25 @@
  */
 package proyectoprogramacion;
 
+import java.io.File;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
 
 /**
  *
- * @author Raimundo
+ * @author Raimundo, Jesus Moris
+ * @version 2016/08/15
  */
-public class MainWindowController 
+public class MainWindowController
 {
+    Stage stage;
     @FXML
     private Button fileButton;
     
@@ -25,13 +31,30 @@ public class MainWindowController
     private MenuItem menuFile;
     
     @FXML
+    private void initialize()
+    {
+        
+    }
+
+    public void setStage(Stage stage)
+    {
+        this.stage = stage;
+    }
+    
+    @FXML
     public void openFile(Event event) 
     {
-        System.out.println("HOLA");
+        System.out.println("Se inicia el evento y abre selector de ficheros.");
         FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("MumoGrafo (*.mmg)", "*.mmg");
         fileChooser.setTitle("Seleccione grafo ");
-        fileChooser.showOpenDialog(((Node)event.getTarget()).getScene().getWindow());
-        
+        fileChooser.getExtensionFilters().add(extFilter);
+        File file = fileChooser.showOpenDialog(this.stage);
+        if(file != null)
+        {
+            System.out.println("Se carga el fichero y se obtiene su informacion.");
+            System.out.println("\nInformación del archivo\n---------------------------\nNombre: " + file.getName() + "\nTamaño: " + file.length() + "\nRuta: " + file.getAbsolutePath() + "\n---------------------------\n");
+        }
     }
      
 
